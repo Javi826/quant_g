0c7150eebca6104edaa5fa98c1c89c252cccee4e
javi <jlahoz.ferrandez@gmail.com>
2025-10-13 14:41:15 +0200

    13-10

--- scripts/utils/ZZ_connect_03.py
+++ scripts/utils/ZZ_connect_03.py
@@ -3,7 +3,9 @@ import ccxt
 # -----------------------------
 # BITGET CONFIG
 # -----------------------------
-
+BITGET_API_KEY    = "bg_afdcb9221ad98efb3b0b7bdd4c236338"
+BITGET_API_SECRET = "0c4214cbfccfb648f841b43ca5d68531c8fb44b75ab271fdd222da9a74ee413f"
+BITGET_API_PASS   = "Cryptobitget86"
 
 # -----------------------------
 # CONNECTION AND SYMBOLS
