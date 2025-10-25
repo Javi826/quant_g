b14c2e4688d75aa3b1564dd61d15e16b76082801
javi <jlahoz.ferrandez@gmail.com>
2025-10-25 18:24:41 +0200

    25-00

--- scripts/utils/ZZ_connect.py
+++ scripts/utils/ZZ_connect.py
@@ -5,13 +5,7 @@ BASE_URL = "https://api.bitget.com"
 # -----------------------------
 # BITGET CONFIG
 # -----------------------------
-BITGET_API_KEY_01    = "bg_afdcb9221ad98efb3b0b7bdd4c236338"
-BITGET_API_SECRET_01 = "0c4214cbfccfb648f841b43ca5d68531c8fb44b75ab271fdd222da9a74ee413f"
-BITGET_API_PASS_01   = "Cryptobitget86"
 
-BITGET_API_KEY_03    = "bg_32bb96c54d766f315346ac7ca3933fa1"
-BITGET_API_SECRET_03 = "5ac75f6e920f045f2bba886e4d22fe2376151681e93f2f0c4a29003afcb091e7"
-BITGET_API_PASS_03   = "Cryptobitget863"
 
 # -----------------------------
 # CONNECTION AND SYMBOLS
